1c6e85b85f624ab401eccc03bcefd58b7f22faa6
marleneeeeeeeee <ma.haberhauer@gmail.com>
2026-05-31 09:52:35 +0200

    Fix1 no unused vars

--- src/app/auth/auth-guard.ts
+++ src/app/auth/auth-guard.ts
@@ -2,7 +2,7 @@ import { inject } from '@angular/core'
 import { CanActivateFn, Router} from '@angular/router';
 import { ApiService} from '../services/api'
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = () => {
   const api = inject(ApiService);
   const router = inject(Router);
   if(!api.isLoggedIn()){
